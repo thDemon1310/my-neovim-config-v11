a4ace74bd499dbb94ea51d4c3dfcca147102383f
Utplaksh Mishra <umishra8770@gmail.com>
2026-03-23 23:22:22 +0530

    added extra keybinding for searching word under cursor telescope plugin to my nvim

--- lua/utplaksh/plugins/telescope.lua
+++ lua/utplaksh/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
       local telescope = require("telescope")
       telescope.setup({
         defaults = {
-          -- path_display = {"smart"},
+          path_display = {"smart"},
         },
       })
 	local builtin = require('telescope.builtin')
@@ -17,5 +17,6 @@ return {
 	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	vim.keymap.set('n', '<leader>fw', builtin.help_tags, { desc = 'Find string under the cursor' })
     end
 }
