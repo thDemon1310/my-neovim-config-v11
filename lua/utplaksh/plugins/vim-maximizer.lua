775682c80f3ecf62e0073317ad24acfc6b2b95bb
Utplaksh Mishra <umishra8770@gmail.com>
2026-03-24 21:52:36 +0530

    corrected the toggle cmd

--- lua/utplaksh/plugins/vim-maximizer.lua
+++ lua/utplaksh/plugins/vim-maximizer.lua
@@ -1,6 +1,6 @@
 return{
   "szw/vim-maximizer",
   keys = {
-    {"<A-w>m",":MazimizerToggle<CR>", desc = "Max/Min window split"}
+    {"<A-w>m",":MaximizerToggle<CR>", desc = "Max/Min window split"}
   },
 }
